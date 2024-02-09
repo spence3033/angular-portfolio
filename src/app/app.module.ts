75272133493d62d69dc121262083e259ac8edd4c
spencer <sbirch3303@gmail.com>
2024-02-09 15:59:36 -0700

    adding header and page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PageComponent } from './page/page.component';
+import { HeaderComponent } from './page/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    PageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
