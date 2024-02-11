381d2155432687d754fe3935bcfbc190db382bd6
spencer <sbirch3303@gmail.com>
2024-02-11 11:20:49 -0700

    adding all expected components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/body/home/home.component';
+import { ResumeComponent } from './page/body/resume/resume.component';
 
 const routes: Routes = [
-  // { path: ''}
+  { path: 'home', component: HomeComponent},
+  { path: 'resume', component: ResumeComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 import { PageComponent } from './page/page.component';
 import { HeaderComponent } from './page/header/header.component';
+import { HomeComponent } from './page/body/home/home.component';
+import { AboutComponent } from './page/body/about/about.component';
+import { ResumeComponent } from './page/body/resume/resume.component';
+import { PortfolioComponent } from './page/body/portfolio/portfolio.component';
+import { ContactComponent } from './page/body/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     PageComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
