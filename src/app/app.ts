de3b476089820a29184fc1e811de7834d723c2d1
spencer <sbirch3303@gmail.com>
2026-05-09 11:48:30 -0700

    add hero, about, skills, projects, contact components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Hero,
+    About,
+    Skills,
+    Projects,
+    Contact
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('angular-portfolio');
-}
+export class App {}
